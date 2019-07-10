6ff3eb9088fcf7379bf60d58558ece549c8b04a6
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-10 13:15:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,40 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    if phrase = "Hi, my name is Sophie."
+    if self.end_with?(".")
       return true
     else
-      phrase = "Hi, my name is Sophie"
       return false
     end
   end
 
   def question?
-    if question = "What's your name?"
+    if self.end_with?("?")
       return true
     else
-       question = "Happy Halloween!"
       return false
     end
   end
 
   def exclamation?
-    if exclamation = "Hi, my name is Sophie!"
+    if self.end_with?("!")
       return true
     else
-      exclamation = "Hi, my name is Sophie"
       return false
     end
   end
 
   def count_sentences
-    if sentence = "one. two. three?"
-      return 3
-    elsif sentence = ""
-      return 0
-    else
-      sentence = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
-      return 4
-    end
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
